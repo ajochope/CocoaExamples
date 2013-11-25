83366ad1466d1783bb1e851d0d32476811fe0f1b
Oscar Calles <ocalles@gmail.com>
2013-11-25 13:14:47 +0100

    Hello world from Cocoa

---
+++ CocoaExamples/CocoaExamples/AppController.h
@@ -0,0 +1,20 @@
+//
+//  AppController.h
+//  CocoaExamples
+//
+//  Created by Ajo on 11/25/13.
+//  Copyright (c) 2013 ocalles@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppController : NSObject{
+@private
+    IBOutlet NSTextField *label;
+    
+    
+}
+
+- (IBAction)sayHello:(id)sender;
+
+@end
---
+++ CocoaExamples/CocoaExamples/AppController.m
@@ -0,0 +1,19 @@
+//
+//  AppController.m
+//  CocoaExamples
+//
+//  Created by Ajo on 11/25/13.
+//  Copyright (c) 2013 ocalles@gmail.com. All rights reserved.
+//
+
+#import "AppController.h"
+
+@implementation AppController
+
+- (IBAction)sayHello:(id)sender
+{
+    [label setStringValue:@"Hello world from Cocoa !!"];
+    
+}
+
+@end
